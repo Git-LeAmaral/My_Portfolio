11609c11b222adc888173707e1c7dcd5f6ec95c2
Leandro Amaral <lramaral02m@gmail.com>
2023-08-22 01:15:18 -0300

    Ajustando responsividade e menu toggle

--- Novo Portifólio/js/script.js
+++ Novo Portifólio/js/script.js
@@ -1,13 +1,14 @@
 let list = document.querySelectorAll('.nav li')
-let menuToggle = document.querySelector('.menuToggle')
-let header = document.querySelector('header')
 
 function active() {
-  list.forEach(i => i.classList.remove('active'))
+  list.forEach((i) => i.classList.remove('active'))
   this.classList.add('active')
 }
 
-list.forEach(i => i.addEventListener('click', active))
+list.forEach((i) => i.addEventListener('click', active))
+
+let menuToggle = document.querySelector('.menuToggle')
+let header = document.querySelector('header')
 
 menuToggle.onclick = function () {
   header.classList.toggle('active')
